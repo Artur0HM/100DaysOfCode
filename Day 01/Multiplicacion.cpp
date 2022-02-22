b99f637c001862383f9630f8d2fef5223c1cb5a5
HURTADO MENDEZ Arturo <hurtadomendez90@gmail.com>
2022-02-21 21:05:35 -0500

    Multiplicacion

---
+++ Day 01/Multiplicacion.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+
+    double numero_uno;
+    double numero_dos;
+
+    cout << "LA MULTIPLICACIÓN DE LOS NÚMEROS ES:" << endl;
+
+    cout << "Ingrese el primer número: ";
+    cin >> numero_uno;
+
+    cout << "Ingrese el segundo número: ";
+    cin >> numero_dos;
+
+    cout << "La multiplicación es: " << numero_uno * numero_dos;
+
+    return 0;
+}
