db7cc4d3e0d4f787914a04c55fbba422779fd28b
HURTADO MENDEZ Arturo <hurtadomendez90@gmail.com>
2022-02-21 20:46:01 -0500

    Suma en c++

---
+++ Day 01/Suma.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main (){
+    double numero_uno;
+    double numero_dos;
+
+    cout << "LA SUMA DE LOS NÚMEROS ES: " <<endl;
+    
+    cout << "Ingrese el primer número: ";
+    cin >> numero_uno;
+
+    cout <<"Ingrese el segundo número: ";
+    cin >> numero_dos;
+
+    cout << "La suma es: " << numero_uno + numero_dos;
+
+    return 0;
+
+}
