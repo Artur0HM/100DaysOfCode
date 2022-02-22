4319c8e013fc6da93a74fe04f8a2b206d69c51ee
HURTADO MENDEZ Arturo <hurtadomendez90@gmail.com>
2022-02-21 20:55:49 -0500

    Resta

---
+++ Day 01/Resta.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main (){
+
+    double numero_uno;
+    double numero_dos;
+
+    cout << "LA RESTA DE LOS NÚMEROS ES: " <<endl;
+
+    cout << "Ingrese el primer número: ";
+    cin >> numero_uno;
+
+    cout << "INgrese el segundo número: ";
+    cin >> numero_dos;
+
+    cout << "La resta es: " << numero_uno - numero_dos;
+    return 0;
+}
