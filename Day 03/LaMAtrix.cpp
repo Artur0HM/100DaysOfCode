ca334fe8a04b77b833e32420e2cdb685587ae10e
HURTADO MENDEZ Arturo <hurtadomendez90@gmail.com>
2022-02-23 15:51:06 -0500

    La matrix

---
+++ Day 03/LaMAtrix.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+
+    string nombre;
+
+    cout << "LA MATRIX" << endl;
+
+    cout << "Ingresa tu nombre: ";
+    cin >> nombre;
+
+    cout << "Ahora estas en la matrix, " << nombre;
+
+    return 0;
+
+}
