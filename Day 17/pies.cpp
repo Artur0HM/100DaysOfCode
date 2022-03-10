6a93f66883617a7dd5fea9e0517831d3a3d8789a
HURTADO MENDEZ Arturo <hurtadomendez90@gmail.com>
2022-03-09 22:40:24 -0500

    Dia 17 de 100

--- Day 17/pies.cpp
+++ Day 17/pies.cpp
@@ -1,18 +1,21 @@
 /*Dada una cantidad expresada en pies, y otra en metros. Determinar la suma pero convertida a pulgadas, a yardas, a metros y a millas por separado. Considere las siguientes equivalencias:
 
-1 milla = 1609 metros, 1 pulgada = 0.0254 metros, 1 yarda = 3 pies, 1 pie = 12 pulgadas.*/
+1 milla = 1609 metros, 1 pulgada = 0.0254 metros, 1 yarda = 3 pies, 1 pie = 12 pulgadas, 1 metro = 3.2808399 pie*/
 
 #include <iostream>
 using namespace std;
 int main (){
 
     int medidaPies;
-    float totalMetros;
     int medidaMetros;
+    float totalMetros;
+    float totalPulgadas;
+
     int milla = 1609;
     float pulgada = 0.0254;
     int yarda = 3;
     int pie = 12;
+    float metro = 3.2808399;
 
     cout << "Ingrese una medida en pies: ";
     cin >> medidaPies;
@@ -21,7 +24,8 @@ int main (){
     cin >> medidaMetros;
 
     totalMetros = ((medidaPies * pie) * pulgada) + medidaMetros;
-
+    totalPulgadas = (medidaMetros * metro);
+    
     cout << "La suma de la medida en pies y la medida en metros es: " << totalMetros << " kilometros" << endl;
     cout << "";
 
