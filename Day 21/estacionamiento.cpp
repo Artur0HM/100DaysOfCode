aaa2348dccc3bb34e6d1fdf33b8a6d4336c96fe0
HURTADO MENDEZ Arturo <hurtadomendez90@gmail.com>
2022-03-14 18:22:05 -0500

    Dia 21 de 100

--- Day 21/estacionamiento.cpp
+++ Day 21/estacionamiento.cpp
@@ -1,31 +1,43 @@
 /*En una playa de estacionamiento cobran S/. 2.00 por hora o fracción los días Lunes, Martes y Miércoles, S/. 2.50 los días Jueves y Viernes, S/. 3.00 los días Sábado y Domingo. Se considera fracción de hora cuando haya pasado de 5 minutos. Diseñe un programa que determine cuánto debe pagar un cliente por su estacionamiento en un solo día de la semana. Si el tiempo ingresado es incorrecto imprima un mensaje de error.*/
 
 #include <iostream>
+#include <stdio.h>
+#include <iomanip>
 using namespace std;
 int main (){
 
-    int DiaDeSemana;
+    string DiaDeSemana;
     int tiempoHoras;
     int tiempoMinutos;
     int primerPrecio = 2;
     float segundoPrecio = 2.50;
     int tercerPrecio = 3;
+    int totalPagarPorHoras;
+    double totalPagarPorMinutos;
+    double totalPagar;
 
-    cout << "1. LUNES" << endl;
-    cout << "2. MARTES" << endl;
-    cout << "3. MIECOLES" << endl;
-    cout << "4. JUEVES" << endl;
-    cout << "5. VIERNES" << endl;
-    cout << "6. SABADO" << endl;
-    cout << "7. DOMINGO" << endl;
+    cout << "INGRESE QUE DÍA ES; " << endl <<endl; 
+    cout << "LUNES - MARTES - MIECOLES - JUEVES - VIERNES - SABADO - DOMINGO" << endl;
     cout << "Ingrese un día de la semana: ";
     cin >> DiaDeSemana;
 
-    if (DiaDeSemana == 1 || DiaDeSemana == 2 || DiaDeSemana == 3)
+    if (DiaDeSemana == "lunes" || DiaDeSemana == "martes" || DiaDeSemana == "miercoles")
     {
-        cout << "Hoy es " << DiaDeSemana << " en tiempo de estacionamiento es " << tiempoHoras << " horas:" << tiempoMinutos << " minutos";
+        cout << "Cuantas horas se quedo: ";
+        cin >> tiempoHoras;
+        cout << "Cuantos minutos se quedo: ";
+        cin >> tiempoMinutos;
+
+        totalPagarPorHoras = tiempoHoras * primerPrecio;
+        totalPagarPorMinutos = tiempoMinutos / 60 * 2;
+        totalPagar = double(totalPagarPorHoras) + double(totalPagarPorMinutos);
+
+    /* cout << "Hoy es " << DiaDeSemana << " el tiempo de estacionamiento es " << tiempoHoras << " horas:" << tiempoMinutos << " minutos" << endl;*/
+
+        cout << "Total a pagar es de : " << totalPagarPorMinutos << " soles";
+
+
     }
     
-
     return 0;
 }
