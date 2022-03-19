83cee22e1049fdcd17929fb6a29aab38f0c065b7
HURTADO MENDEZ Arturo <hurtadomendez90@gmail.com>
2022-03-18 21:59:03 -0500

    Dia 26 de 100

---
+++ Day 26/cuentaBancaria.cpp
@@ -0,0 +1,15 @@
+/*Cuenta Bancaria*/
+
+#include <iostream>
+using namespace std;
+int main (){
+
+    int edad;
+    string nacinalidad;
+    string nombres;
+    string apellidos;
+    string direccion;
+    char tipoDeDocumento;
+
+    return 0;
+}
