53076bd5a574eb9a1b7d6f3d3b579daa326ebad1
HURTADO MENDEZ Arturo <hurtadomendez90@gmail.com>
2022-04-09 23:15:02 -0500

    Dia 34 - 35 de 100

--- Day 34/introdicirNumeros.cpp
+++ Day 34/introdicirNumeros.cpp
@@ -2,33 +2,36 @@
 using namespace std;
 int main (){
 
-    int numero;
+    int valorIngresar, numero, numeroDos;
 
     cout << "*******************************************" << endl;
     cout << "***   CUANTOS VALORES VA A INTRODUCIR   ***" << endl;
     cout << "*******************************************" << endl;
 
     cout << "Cuantos valores vas ingresar: ";
-    cin >> numero;
+    cin >> valorIngresar;
 
-    if (numero > 0)
+    if (valorIngresar > 0)
     {
-        cout << "Vas ingresar " << numero << " números." << endl;
+        cout << "Vas ingresar " << valorIngresar << " números." << endl;
+            cout << "Ingrese el primer número: ";
+            cin >> numero;
 
-        cout << "Ingrese el primer número: ";
-        cin >> numero;
-
-            for (int i = numero; i <= numero; i++)
-            {
-                cout << "Ingrese un número mayor a: ";
-                cin >> numero;
+                for (int i = 1; i < valorIngresar; i++)
+                {
+                    cout << "Ingrese un número mayor al primer número que es " << numero << ": ";
+                    cin >> numeroDos;
                 
-                    
+                        if (numeroDos < numero )
+                        {
+                            cout << numeroDos << " no es mayor al primer número " << numero << endl;
+                        }                    
             }
-            
+        cout << "Gracias por su colaboración.";
+        
     }
     else {
-        cout << "El número que ingrese debe ser mayoe a CERO.";
+        cout << "El número que ingrese es "<< numero << " debe ser mayoe a CERO.";
     }
     
     return 0;
